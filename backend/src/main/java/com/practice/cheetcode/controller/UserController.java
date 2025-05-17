006b14df5a4a1a45169cd8df7be45621f7337ca2
Chandan Shaw <dev.chandan.shaw@gmail.com>
2025-05-17 22:32:17 +0530

    Update UserController

--- backend/src/main/java/com/practice/cheetcode/controller/UserController.java
+++ backend/src/main/java/com/practice/cheetcode/controller/UserController.java
@@ -13,6 +13,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Optional;
 
@@ -58,5 +60,10 @@ public class UserController {
         loginResponse.setLastName(user.getLastName());
         loginResponse.setToken(token);
         return ResponseEntity.status(HttpStatus.CREATED).body(loginResponse);
+    }
+
+    @GetMapping("/ping")
+    public ResponseEntity<?> ping() {
+        return ResponseEntity.ok(new Date());
     }
 }
