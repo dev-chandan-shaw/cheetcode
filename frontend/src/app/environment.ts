5fd1f7ea0ed0a6c36f69e00f55547dbb7c7b7f6c
Chandan Shaw <dev.chandan.shaw@gmail.com>
2025-05-23 21:17:27 +0530

    fix: update the api url

--- frontend/src/app/environment.ts
+++ frontend/src/app/environment.ts
@@ -1,7 +1,7 @@
 const environment = {
   production: false,
   api: 'https://cheetcode-ns47.onrender.com/api',
-    api: 'http://localhost:8080/api',
+    // api: 'http://localhost:8080/api',
 };
 
 export default environment;
