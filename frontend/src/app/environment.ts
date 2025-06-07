11dd50933cce19011aab7006e42b02a868042dbd
Chandan Shaw <dev.chandan.shaw@gmail.com>
2025-06-08 04:38:28 +0530

    change style issue and google redirect url wrong

--- frontend/src/app/environment.ts
+++ frontend/src/app/environment.ts
@@ -2,7 +2,7 @@ const environment = {
   production: false,
   api: 'https://cheetcode-ns47.onrender.com/api',
     // api: 'http://localhost:8080/api',
-    googleOAuthRedirectUrl: 'https://cheetcode-ns47.onrender.com/oauth2/authorization/google',
+    googleOAuthRedirectUrl: 'https://cheetcode-ns47.onrender.com/login/oauth2/code/google',
     // googleOAuthRedirectUrl: 'http://localhost:8080/oauth2/authorization/google',
 };
 
